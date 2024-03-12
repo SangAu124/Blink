eaf279e8eaf3312f2280ce6ed6be005cf467e5e5
모던텍 <moderntec_sw@moderntec.kr>
2024-03-12 09:43:07 +0900

    🎉 :: Init Commit

---
+++ Blink/BlinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkApp.swift
+//  Blink
+//
+//  Created by moderntec_sw on 2024/03/12.
+//
+
+import SwiftUI
+
+@main
+struct BlinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
