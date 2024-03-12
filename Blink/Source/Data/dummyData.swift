1625753ecf809b4a857a6322a29192160170e184
김상은 <dev.sangau20@gmail.com>
2024-03-12 10:12:35 +0900

    🚚 :: Basic File Structure

---
+++ Blink/Source/Data/dummyData.swift
@@ -0,0 +1,8 @@
+//
+//  dummyData.swift
+//  Blink
+//
+//  Created by moderntec_sw on 2024/03/12.
+//
+
+import Foundation
---
+++ Blink/Source/Domain/dummyDomain.swift
@@ -0,0 +1,8 @@
+//
+//  dummyDomain.swift
+//  Blink
+//
+//  Created by moderntec_sw on 2024/03/12.
+//
+
+import Foundation
